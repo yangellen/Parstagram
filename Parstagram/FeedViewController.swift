9f5e1af1ae5e6b7f2b11568141d513d752af8d5a
Ellen Yang <yange@oregonstate.edu>
2021-03-19 15:36:36 -0700

    newest post display on top

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -27,6 +27,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
       super.viewDidAppear(animated)
 
       let query = PFQuery(className: "Post")
+      query.order(byDescending: "createdAt")
       query.includeKey("author")
       query.limit = 20
 
