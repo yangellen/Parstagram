15d789cb4f100a81481603c3a42330a955fdcd69
Ellen Yang <yange@oregonstate.edu>
2021-03-23 21:31:13 -0700

    User can stay logged in across restarts

--- Parstagram/SceneDelegate.swift
+++ Parstagram/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,7 +17,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
       // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
       // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
+      //add user persistance across app restarts
+      if PFUser.current() != nil{
+        login()
+      }
       guard let _ = (scene as? UIWindowScene) else { return }
+   }
+
+   func login(){
+      let storyboard = UIStoryboard(name: "Main", bundle: nil)
+      //view controller currently being set in Storyboard as default will be overriden
+      window?.rootViewController = storyboard.instantiateViewController(identifier: "FeedNavigationController")
    }
 
    func sceneDidDisconnect(_ scene: UIScene) {
